3f737fe849c720209c9ad0e99d09e2c968a87c4e
Argus <serioga.com@gmail.com>
2021-06-24 13:33:07 +0300

    Добавил в чоиз прочерки

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from .models import Questions, OrderServices, ServicesAndPrice
-from django.forms import ModelForm, TextInput, Textarea, Select, ChoiceField
+from django.forms import ModelForm, TextInput, Textarea, Select
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -9,21 +9,27 @@ class QuestionForm(ModelForm):
         fields = ['tel', 'name', 'text_questions']
 
         widgets = {
-            'tel': TextInput(attrs={
-                'class': 'CreateTask',
-                'placeholder': _("Номер телефону"),
-                'type': 'tel',
-                'pattern': "[+38]{0,3}[0-9]{3}[0-9]{3}[0-9]{2}[0-9]{2}",
-            }),
-            'name': TextInput(attrs={
-                'type': 'text',
-                'placeholder': _("Ім'я"),
-                'class': "CreateTask",
-            }),
-            'text_questions': Textarea(attrs={
-                'placeholder': _("Питання"),
-                'class': "CreateTaskText",
-            }),
+            'tel': TextInput(
+                attrs={
+                    'class': 'CreateTask',
+                    'placeholder': _("Номер телефону"),
+                    'type': 'tel',
+                    'pattern': "[+38]{0,3}[0-9]{3}[0-9]{3}[0-9]{2}[0-9]{2}",
+                }
+            ),
+            'name': TextInput(
+                attrs={
+                    'type': 'text',
+                    'placeholder': _("Ім'я"),
+                    'class': "CreateTask",
+                }
+            ),
+            'text_questions': Textarea(
+                attrs={
+                    'placeholder': _("Питання"),
+                    'class': "CreateTaskText",
+                }
+            ),
         }
 
 
@@ -33,23 +39,28 @@ class OrderServicesForm(ModelForm):
         fields = ['tel_service', 'name_service', 'choice_service', 'text_service']
 
         cho = set()
+        cho.add(('------', '---------'))
         c = 1
         for j in ServicesAndPrice.objects.all():
             j = str(j)
             cho.add((j, j))
 
         widgets = {
-            'tel_service': TextInput(attrs={
-                'class': 'CreateTask',
-                'placeholder': _("Номер телефону"),
-                'type': 'tel',
-                'pattern': "[+38]{0,3}[0-9]{3}[0-9]{3}[0-9]{2}[0-9]{2}",
-            }),
-            'name_service': TextInput(attrs={
-                'class': "CreateTask",
-                'type': 'text',
-                'placeholder': _("Ім'я"),
-            }),
+            'tel_service': TextInput(
+                attrs={
+                    'class': 'CreateTask',
+                    'placeholder': _("Номер телефону"),
+                    'type': 'tel',
+                    'pattern': "[+38]{0,3}[0-9]{3}[0-9]{3}[0-9]{2}[0-9]{2}",
+                }
+            ),
+            'name_service': TextInput(
+                attrs={
+                    'class': "CreateTask",
+                    'type': 'text',
+                    'placeholder': _("Ім'я"),
+                }
+            ),
             'choice_service': Select(
                 attrs={
                     'class': 'CreateTask select',
